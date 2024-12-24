4d668ff19c1b465a810964266b36cb1537650b69
Tuntikorn Visoothikul <visoothikul.t@gmail.com>
2024-12-24 20:29:36 +0700

    first commit

---
+++ exam3.js
@@ -0,0 +1,12 @@
+// โจทย์: คุณมีออบเจ็กต์ตัวอย่างดังนี้:
+// const person = {
+//     name: 'John',
+//     age: 25,
+//     city: 'Bangkok',
+//     job: 'Developer',
+//   };
+
+
+// 3.1 ลบ key job ออกจากออบเจ็กต์
+// 3.2 ใช้การ assign (หรือ destructure) เพื่อสร้างออบเจ็กต์ใหม่ที่เพิ่ม key country พร้อมค่าเป็น 'Thailand'
+// 3.3 อธิบายและทดลองการเปลี่ยนค่าในออบเจ็กต์ที่ประกาศด้วย const (เพื่ออธิบายเรื่อง reference address memory)
---
+++ exam4.js
@@ -0,0 +1,10 @@
+// โจทย์: ใช้ axios เพื่อ get ค่าจาก API
+// const baseUrl = 'https://jsonplaceholder.typicode.com/';
+// GET	/posts
+// GET	/posts/1
+// GET	/posts/1/comments
+// GET	/comments?postId=1
+// POST	/posts
+// PUT	/posts/1
+// PATCH	/posts/1
+// DELETE	/posts/1
